15aaa51137711634909890feac60ca44d88dbee4
Ekaterina Levshchanova <ELevshhanova@IBS.RU>
2020-04-30 15:15:07 +0400

    add popup

---
+++ js/popup.js
@@ -0,0 +1,18 @@
+let popup = document.getElementById("popup");
+let closeSpan = document.getElementById("close");
+
+function showAdd() {
+    popup.className = "popup-show";
+}
+
+window.onclick = function(event) {
+    if (event.target == popup) {
+        closePopup();
+    }
+}
+closeSpan.onclick = function() {
+    closePopup();
+}
+function closePopup() {
+    popup.className = "popup-hide";
+}
