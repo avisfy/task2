ceb5246cfb6398911bd62d2b55312e9f839d5bc7
avisfy <avisfy@mail.ru>
2020-05-03 17:17:19 +0400

    add popup animation

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,17 @@
 let popup = document.getElementById("popup");
 let closeSpan = document.getElementById("close");
 
-function showAdd() {
-    popup.className = "popup-show";
-}
-
-window.onclick = function (event) {
-    if (event.target == popup) {
-        closePopup();
+function popupAction() {
+    if (popup.className === "popup-show") {
+        popup.className = "popup-hide";
+        document.getElementById("n-btn-add").value = "Add news!";
+    } else {
+        popup.className = "popup-show";
+        document.getElementById("n-btn-add").value = "Close";
     }
 }
-closeSpan.onclick = function () {
-    closePopup();
-}
+
+
 
 function closePopup() {
     popup.className = "popup-hide";
