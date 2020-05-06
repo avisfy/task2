2516c0f92f01d704ce64f2300e8e1762fd8a70fb
Ekaterina Levshchanova <avisfy@mail.ru>
2020-05-06 15:51:14 +0400

    fix some bugs, refactor js files structure

---
+++ js/blocks.js
@@ -0,0 +1,19 @@
+export function createBlock(tag, text, classname) {
+    const block = document.createElement(tag);
+    if (classname !== undefined) {
+        block.className = classname;
+    }
+    if (text !== "") {
+        block.textContent = text;
+    }
+    return block;
+}
+
+export function createImageBlock(classname, ref) {
+    debugger;
+    const imageBlock = document.createElement("img");
+    imageBlock.className = classname;
+    imageBlock.src = ref;
+    imageBlock.onerror = () => imageBlock.src = "images/errorimg.jpg";
+    return imageBlock;
+}
